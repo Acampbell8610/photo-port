9730340d056747ce6370eddff40d224581a06bd4
acamp <acampbell8610@gmail.com>
2022-05-01 20:17:32 -0500

    added hooks and gallery

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,6 +6,9 @@ function About() {
         <section>
             <h1 id="about">Who am I?</h1>
             <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fortasse id optimum, sed ubi illud: Plus semper voluptatis? Itaque fecimus. Et harum quidem rerum facilis est et expedita distinctio. Sed fac ista esse non inportuna; Duo Reges: constructio interrete. Efficiens dici potest.
+
+            </p>
         </section>
   
     );
